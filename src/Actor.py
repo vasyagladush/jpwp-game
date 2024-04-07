7d5c8dcf05b7f19748d3a6e001863ec6d482cfe4
vasyagladush <vasylyi2004@gmail.com>
2024-04-07 15:29:59 +0200

    add documentation to Actor.render method

--- src/Actor.py
+++ src/Actor.py
@@ -14,6 +14,16 @@ class Actor(Renderable):
 
     @override
     def render(self, position: Optional[Vector] = None) -> None:
+        """A function to render the actor on the singleton Display object.
+        If a position is passed in, it will render the actor at that position.
+        Otherwise, it will render the actor at the position of the actor.
+
+        Args:
+            ``position (Optional[Vector], optional)``: position to render actor at (instead of actor's object position). Defaults to ``None``.
+
+        Returns:
+            ``None``
+        """
         if position is not None:
             return super().render(position)
         return super().render(self.position)
